f9b63ebf88a369687edf75ffc4d8d6a62c038e65
berend <berend@LT.beer>
2020-11-23 19:49:09 +0100

    commited hw11

--- src/main/java/space/harbour/hw11/NioChatServer.java
+++ src/main/java/space/harbour/hw11/NioChatServer.java
@@ -1,42 +1,126 @@
-package space.harbour.hw11;
+package space.harbour.java.hw11;
 
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * This is a simple NIO based server.
+ */
 public class NioChatServer {
-    private Set<NioChatHandler> clients = ConcurrentHashMap.newKeySet();
+    private static final int PORT = 9093;
 
-    public NioChatServer(int portNumber) {
-        try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
-            System.out.println("Waiting on port : " + portNumber + "...");
+    private Selector selector;
+    private ByteBuffer buf = ByteBuffer.allocate(256);
+    private final InetSocketAddress listenAddress;
 
-            while (true) {
-                Socket clientSocket = serverSocket.accept();
-                NioChatHandler client = new NioChatHandler(this, clientSocket);
-                clients.add(client);
-                client.start();
-            }
+    public static void main(String[] args) throws Exception {
+        try {
+            new NioChatServer("localhost", 9093).startServer();
         } catch (IOException e) {
-            System.out.println("Server failed on portNumber " + portNumber);
+            e.printStackTrace();
         }
     }
 
-    public synchronized void broadcast(String message) {
-        System.out.println("New message -> " + message);
-        for (NioChatHandler chat: clients) {
-            chat.sendMessage(message);
+    public NioChatServer(String address, int port) throws IOException {
+        listenAddress = new InetSocketAddress(address, PORT);
+    }
+
+    private void startServer() throws IOException {
+        this.selector = Selector.open();
+        ServerSocketChannel serverChannel = ServerSocketChannel.open();
+        serverChannel.configureBlocking(false);
+
+        // bind server socket channel to port
+        serverChannel.socket().bind(listenAddress);
+        serverChannel.register(this.selector, SelectionKey.OP_ACCEPT);
+
+        System.out.println("Server started on port >> " + PORT);
+
+        while (true) {
+            // wait for events
+            int readyCount = selector.select();
+            if (readyCount == 0) {
+                continue;
+            }
+
+            // process selected keys...
+            Set<SelectionKey> readyKeys = selector.selectedKeys();
+            Iterator iterator = readyKeys.iterator();
+            while (iterator.hasNext()) {
+                SelectionKey key = (SelectionKey) iterator.next();
+
+                // Remove key from set so we don't process it twice
+                iterator.remove();
+
+                if (!key.isValid()) {
+                    continue;
+                }
+
+                if (key.isAcceptable()) { // Accept client connections
+                    this.handleAccept(key);
+                } else if (key.isReadable()) { // Read from client
+                    this.handleRead(key);
+                }
+            }
         }
     }
 
-    public void clientDisconnected(NioChatHandler chat) {
-        clients.remove(chat);
-        broadcast("Chat member " + chat.getName() + " left");
+    private final ByteBuffer welcomeBuf = ByteBuffer.wrap("Welcome to NioChat!\n".getBytes());
+
+    private void handleAccept(SelectionKey key) throws IOException {
+        SocketChannel sc = ((ServerSocketChannel) key.channel()).accept();
+        String address = sc.socket().getInetAddress().toString() + ":" + sc.socket().getPort();
+        sc.configureBlocking(false);
+        sc.register(selector, SelectionKey.OP_READ, address);
+        sc.write(welcomeBuf);
+        welcomeBuf.rewind();
+        System.out.println("accepted connection from: " + address);
     }
 
-    public static void main(String[] args) {
-        new NioChatServer(4444);
+    private void handleRead(SelectionKey key) throws IOException {
+        SocketChannel ch = (SocketChannel) key.channel();
+        StringBuilder sb = new StringBuilder();
+
+        buf.clear();
+        int read = 0;
+        while ((read = ch.read(buf)) > 0) {
+            buf.flip();
+            byte[] bytes = new byte[buf.limit()];
+            buf.get(bytes);
+            sb.append(new String(bytes));
+            buf.clear();
+        }
+        String msg;
+        if (read < 0) {
+            msg = key.attachment() + " left the chat.\n";
+            ch.close();
+        } else {
+            msg = key.attachment() + ": " + sb.toString();
+        }
+
+        System.out.println(msg);
+        if (sb.toString().equals("BYE")) {
+            key.cancel();
+        }
+        broadcast(msg);
     }
+
+    private void broadcast(String msg) throws IOException {
+        ByteBuffer msgBuf = ByteBuffer.wrap(msg.getBytes());
+        for (SelectionKey key : selector.keys()) {
+            if (key.isValid() && key.channel() instanceof SocketChannel) {
+                SocketChannel sch = (SocketChannel) key.channel();
+                sch.write(msgBuf);
+                msgBuf.rewind();
+            }
+        }
+    }
+
 }
